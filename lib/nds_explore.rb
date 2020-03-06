d87879c8b95a5e5979a49fe6088da192762a463f
Wendy Raven McNair <wen.mcnair@gmail.com>
2020-03-06 23:29:13 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -2,19 +2,19 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 # Call the method directors_database to retrieve the NDS
 
-#<<<<<<< HEAD
+<<<<<<< HEAD
 require 'pp'
-#=======
+=======
 
-#>>>>>>> be3ce5b23a736a293613d4f51d62150bb9207ae4
+>>>>>>> be3ce5b23a736a293613d4f51d62150bb9207ae4
 
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
 
-#<<<<<<< HEAD
-#=======
+<<<<<<< HEAD
+=======
     require 'pp'
-#>>>>>>> be3ce5b23a736a293613d4f51d62150bb9207ae4
+>>>>>>> be3ce5b23a736a293613d4f51d62150bb9207ae4
     nds
     pp nds
 
@@ -27,13 +27,13 @@ pretty_print_nds(directors_database)
 def print_first_directors_movie_titles
 
 
-#<<<<<<< HEAD
+<<<<<<< HEAD
       counter = 0
       while counter < directors_database[0][:movies].length do
         puts directors_database[0][:movies][counter][:title]
         counter += 1
       end
 
-#=======
-#>>>>>>> be3ce5b23a736a293613d4f51d62150bb9207ae4
+=======
+>>>>>>> be3ce5b23a736a293613d4f51d62150bb9207ae4
 end
